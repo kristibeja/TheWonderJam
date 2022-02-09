9dc5eb5068d744d537872966bdfcce515d5c9c3d
unknown <kristibeja3@gmail.com>
2022-02-09 20:57:41 +0100

    form check

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function validateForm(){
+    console.log("Kristi has a lot of work to do!")
+    // get value of name input 
+    // get value of email input 
+    // get value of message textarea 
+    // if value != null submit 
+    // else show validation error messages 
+}
