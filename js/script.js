d642b95552f6c061512e95c583dc877b6cc9d7b8
unknown <kristibeja3@gmail.com>
2022-02-09 21:59:38 +0100

    form

--- js/script.js
+++ js/script.js
@@ -6,3 +6,51 @@ function validateForm(){
     // if value != null submit 
     // else show validation error messages 
 }
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// On checkbox click, open modal
+var chb = document.getElementById("myCheckbox");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get the button inside modal
+var inner_btn = document.getElementById("myModalBtn");
+
+// When the user clicks the button, open the modal 
+chb.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When done button clicked, close the modal
+inner_btn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Enable submit button on checkbox click
+
+function enable_btn(termsCheckBox){
+    //If the checkbox has been checked
+    if(termsCheckBox.checked){
+        //Set the disabled property to FALSE and enable the button.
+        document.getElementById("myBtn").disabled = false;
+    } else{
+        //Otherwise, disable the submit button.
+        document.getElementById("myBtn").disabled = true;
+    }
+}
