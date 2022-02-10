6eb56303c8565dfdf97c8b209b8c825071de917f
unknown <kristibeja3@gmail.com>
2022-02-10 08:42:28 +0100

    form validation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,94 @@
+ // get form by id
+ const form = document.getElementById('form');
+ // get value of name input 
+ const name = document.getElementById('name');
+ // get value of email input 
+ const email = document.getElementById('email');
+ // get value of message textarea 
+ const message = document.getElementById('message');
+
+ form.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   checkInputs();
+ });
+
+  function checkInputs(){
+    const nameValue =  name.value.trim();
+    const emailValue = email.value.trim();
+    const messageValue = message.value.trim(); //removes extra space
+
+    if(nameValue === ''){
+      //error
+      setErrorFor(name, 'Please fill your name');
+    } else {
+      //success
+      setSuccessFor(name);
+    }
+
+
+    if(emailValue === ''){
+      //error
+      setErrorFor(email, 'Email can not be blank');
+    } else {
+      //success
+      setSuccessFor(email);
+    }
+
+
+    if(messageValue === ''){
+      //error
+      setErrorFor(message, 'Message text can not be blank');
+    } else {
+      //success
+      setSuccessFor(message);
+    }
+
+  }
+
+    function setErrorFor(input, message){
+      const formGroup = input.parentElement; //form-grup class
+      // const error_msg = document.querySelector('.error-msg');
+      const error_msg = document.getElementsByClassName('error-msg');
+
+      //shtojme tekstin e errorit
+      error_msg.innerHTML = message;
+
+      // shtojme klasen 
+      formGroup.className = 'form-group error';
+
+    }
+
+    function setSuccessFor(input){
+      const formGroup = input.parentElement;
+      formGroup.className = 'form-group success';
+    }
+
+
+
 function validateForm(){
-    console.log("Kristi has a lot of work to do!")
-    // get value of name input 
-    // get value of email input 
-    // get value of message textarea 
+    console.log("Kristi has a lot of work to do!");
+
+    console.log("Kristi did an amazing job!");
+
     // if value != null submit 
     // else show validation error messages 
+
+    // form.addEventListener('submit', (e) => {
+    //   let messages = []
+    //   if (name.value === '' || name.value == null){
+    //     messages.push('Name is required')
+    //   }
+
+    //   if (password)
+
+    //   if (messages.length > 0){
+    //     e.preventDefault()
+    //     errorElement.innerText = messages.join(', ')
+    //   }
+    // })
+
+
 }
 
 
@@ -43,7 +127,6 @@ window.onclick = function(event) {
 }
 
 // Enable submit button on checkbox click
-
 function enable_btn(termsCheckBox){
     //If the checkbox has been checked
     if(termsCheckBox.checked){
